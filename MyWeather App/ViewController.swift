fccb6433bafbb82f04b932f605bf762678e39340
Martin <martindu@umich.edu>
2015-01-05 20:27:19 -0800

    First Working Draft of Weather App

--- MyWeather App/ViewController.swift
+++ MyWeather App/ViewController.swift
@@ -7,9 +7,62 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var enteredCity: UITextField!
+    
+    @IBOutlet var weatherReport: UILabel!
+    
+    @IBAction func pressedButton(sender: AnyObject) {
+
+    var finalReport = " "
+        
+        if(enteredCity.text != "" )
+        {
+            var urlString = "http://www.weather-forecast.com/locations/" + enteredCity.text.stringByReplacingOccurrencesOfString(" ", withString: "") + "/forecasts/latest"
+        
+            var url = NSURL(string: urlString)
+        
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!){ (data, response, error) in
+                
+
+            var urlContent = NSString (data: data, encoding: NSUTF8StringEncoding)!
+        
+            if urlContent.containsString("<span class=\"phrase\">"){
+                
+            
+            var contentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+        
+            var newContentArray = (contentArray[1] as NSString).componentsSeparatedByString("</span>")
+            
+        
+        
+            finalReport = (newContentArray[0] as String).stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+        }
+        else{
+                finalReport = "City Not Found!"
+        }
+            
+        dispatch_async(dispatch_get_main_queue()){
+            self.weatherReport.text = finalReport
+                }
+        }
+        
+        task.resume()
+        
+        }
+        else{
+        
+            weatherReport.text = "Please enter a city!"
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
